31740d46630675ec8588e26bece9d2b8a5c7f7e6
usha-s <PI97@INLTD09296.global.tesco.org>
2015-12-28 16:35:24 +0530

    symbolic links
    updated symbolic links to access nginx files from host. Also checking in
    default nginx files with changes in title

--- Vagrant/provision.sh
+++ Vagrant/provision.sh
@@ -1,4 +1,8 @@
 # -y does not ask for confirmation since this is automatic provisioning tool
 apt-get -y update
 apt-get -y install nginx
+
+rm -rf /usr/share/nginx/html
+ln -s /vagrant/html /usr/share/nginx/html
+
 service nginx start
